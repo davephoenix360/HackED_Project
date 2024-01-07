b5ee9120bac4b4a20e8c2c12e277b97fa398b29a
davephoenix360 <davediepreye05@gmail.com>
2024-01-07 10:40:13 -0700

    Direction Feature Done

--- Final_Test.ino
+++ Final_Test.ino
@@ -6,7 +6,7 @@
 
 int receiver = 2; // Signal Pin of IR receiver to Arduino Digital Pin 11
 /*-----( Declare objects )-----*/
-//IRrecv irrecv(receiver);     // create instance of 'irrecv'
+IRrecv irrecv(receiver);     // create instance of 'irrecv'
 //vairable uses to store the last decodedRawData
 uint32_t last_decodedRawData = 0;
 
@@ -15,7 +15,8 @@ const int Echo = 10;
 const int Trig = 11;
 const int motor = 12;
 int Speed = 30;
-int spoint = 103;
+int spoint = 90;
+int BUZZER_PIN = 13;
 Servo m1;
 Servo m2;
 char value;
@@ -28,56 +29,35 @@ int L1 = 0;
 int R1 = 0;
 Servo servo;
 
-
-void translateIR() // takes action based on IR code received
-{
-  // Check if it is a repeat IR code 
-  if (irrecv.decodedIRData.flags)
-  {
-    //set the current decodedRawData to the last decodedRawData 
-    irrecv.decodedIRData.decodedRawData = last_decodedRawData;
-    Serial.println("REPEAT!");
-  } else
-  {
-    //output the IR code on the serial monitor
-    Serial.print("IR code:0x");
-    Serial.println(irrecv.decodedIRData.decodedRawData, HEX);
-  }
-  //map the IR code to the remote key
-  switch (irrecv.decodedIRData.decodedRawData)
-  {
-    case 0xB847FF00: Stop(); break;
-    case 0xBB44FF00: left();    break;
-    case 0xBC43FF00: right();   break;
-    case 0xEA15FF00: backward();    break;
-    case 0xB946FF00: forward();    break;
-
-    default:
-      break;;
-  }// End Case
-  //store the last decodedRawData
-  last_decodedRawData = irrecv.decodedIRData.decodedRawData;
-  delay(500); // Do not get immediate repeat
-} //END translateIR
+Servo sg90;                   //initializing a variable for servo named sg90
+int initial_position = 90;    //Declaring the initial position at 90
+int LDR1 = A1;                //Pin at which LDR is connected
+int LDR2 = A2;                //Pin at which LDR is connected
+int error = 100;                //initializing variable for error
+int servopin=7;
 
 void setup() {
-  Serial.begin(9600);
+ Serial.begin(9600);
   m1.attach(8);
   m2.attach(9);
   pinMode(Trig, OUTPUT);
   pinMode(Echo, INPUT);
   servo.attach(motor);
-    //m1.write(80);
-  //m2.write(80);
   irrecv.enableIRIn(); // Start the receiver
+  pinMode(BUZZER_PIN, OUTPUT); // Set the buzzer pin as an output
+  sg90.attach(servopin);  // attaches the servo on pin 9
+  pinMode(LDR1, INPUT);   //Making the LDR pin as input
+  pinMode(LDR2, INPUT);
+  sg90.write(initial_position);   //Move servo at 90 degree
+  delay(2000);                   // giving a delay of 2 seconds
 
 }
 
 void loop() {
-  directional_move();
+  //directional_move();
+  solarPanel();
   //remote_control();  
   //Obstacle();
-  //Serial.println("yeah b");
 }
 
 void remote_control(){
@@ -146,6 +126,7 @@ void directional_move(){
   }
 }
 
+
 void Obstacle() {
   distance = ultrasonic();
   if (distance <= 5) {
@@ -172,6 +153,32 @@ void Obstacle() {
   } else {
     forward();
   }
+}
+
+void playTone(int frequency, int duration) {
+  tone(BUZZER_PIN, frequency, duration); // Generate tone at the specified frequency for the given duration
+  delay(duration + 50); // Add a small delay for a better separation between tones
+  noTone(BUZZER_PIN); // Turn off the buzzer
+}
+void tone1(){
+  playTone(500, 500);
+  delay(500);
+}
+
+void tone2(){
+  playTone(1000, 500);
+  delay(500);
+  playTone(1000, 500);
+  delay(500);
+}
+
+void tone3(){
+  playTone(1500, 500);
+  delay(500);
+  playTone(1500, 500);
+  delay(500);
+  playTone(1500, 500);
+  delay(500);
 }
 
 // Ultrasonic sensor distance reading function
@@ -217,4 +224,63 @@ int leftsee() {
   delay(800);
   Right = ultrasonic();
   return Right;
+}
+
+void translateIR() // takes action based on IR code received
+{
+  // Check if it is a repeat IR code 
+  if (irrecv.decodedIRData.flags)
+  {
+    //set the current decodedRawData to the last decodedRawData 
+    irrecv.decodedIRData.decodedRawData = last_decodedRawData;
+    Serial.println("REPEAT!");
+  } else
+  {
+    //output the IR code on the serial monitor
+    Serial.print("IR code:0x");
+    Serial.println(irrecv.decodedIRData.decodedRawData, HEX);
+  }
+  //map the IR code to the remote key
+  switch (irrecv.decodedIRData.decodedRawData)
+  {
+    case 0xB847FF00: Stop(); break;
+    case 0xBB44FF00: left();    break;
+    case 0xBC43FF00: right();   break;
+    case 0xEA15FF00: backward();    break;
+    case 0xB946FF00: forward();    break;
+
+    default:
+      break;;
+  }// End Case
+  //store the last decodedRawData
+  last_decodedRawData = irrecv.decodedIRData.decodedRawData;
+  delay(500); // Do not get immediate repeat
+} //END translateIR
+
+
+void solarPanel(){
+  int Right1 = analogRead(LDR1); // reading value from LDR 1
+  int Right2 = analogRead(LDR2); // reading value from LDR 2
+  int diff1= abs(Right1 - Right2);   // Calculating the difference between the LDR's
+  int diff2= abs(Right2 - Right1);
+  if((diff1 <= error) || (diff2 <= error)) {
+    //if the difference is under the error then do nothing
+  } else {    
+    if(Right1 < Right2)
+    {
+      initial_position = --initial_position;  //Move the servo towards 0 degree
+      Serial.println("right");
+      sg90.write(initial_position);
+    }
+    if(Right1 > Right2) 
+    {
+      initial_position = ++initial_position; //Move the servo towards 180 degree
+      Serial.println("left");
+      sg90.write(initial_position);
+    }
+  }
+  // write the position to servo
+  Serial.println(Right1);
+  Serial.println(Right2);
+  delay(100);
 }
